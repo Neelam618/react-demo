9f7179c42cfdf404832c24b10b2f381893207171
Neelam618 <neelamchavan410@gmail.com>
2021-05-14 16:06:52 +0530

    display input value on btn click using useState

--- src/components/GetInputValue.js
+++ src/components/GetInputValue.js
@@ -2,14 +2,16 @@ import {useState} from 'react'
 
 export default function GetInputValue() {
     const [data, setData] = useState('Type something...');
+    const [print, setPrint] = useState(false)
     
     function getData(val) {
         setData(val.target.value)
+        setPrint(false)
     }
     return (
         <div>
-            <input type="text" onChange={getData}/>
-            <h2>{data}</h2>
+             { print? <h2>{data}</h2> : null }
+             <input type="text" onChange={getData}/><button onClick={() => setPrint(true)}>Print data</button>
         </div>
     )
 }
