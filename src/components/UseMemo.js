7d5019518e6e9bf7472a5ecb3df6fdb73123172f
Neelam618 <neelamchavan410@gmail.com>
2021-05-15 19:27:04 +0530

    useMemo used

--- src/components/UseMemo.js
+++ src/components/UseMemo.js
@@ -1,13 +1,13 @@
-import React, {useMemo, useState} from 'react'
+import React, { useMemo, useState } from 'react'
 
 export default function UseMemo() {
-    const [count, setCount] =useState(0)
-    const [item, setItem] =useState(10)
+    const [count, setCount] = useState(0)
+    const [item, setItem] = useState(10)
 
     const multiplyCountMemo = useMemo(function multiplyCount() {
         console.warn("multiplyCount function called")
-        return count*10
-    }, [count]) 
+        return count * 10
+    }, [count])
 
     return (
         <div>
@@ -16,8 +16,8 @@ export default function UseMemo() {
             <h2>Item: {item}</h2>
             <h2>Multiply count: {multiplyCountMemo}</h2>
 
-            <button onClick={() => setCount(count+1)}>Update Count</button>
-            <button onClick={() => setItem(item*2)}>Update Item</button>
+            <button onClick={() => setCount(count + 1)}>Update Count</button>
+            <button onClick={() => setItem(item * 2)}>Update Item</button>
         </div>
     )
 }
